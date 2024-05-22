bfdc072919d8366dd2ef775573b88ac5fa4c88e4
Mozahedul <mozahed001@gmail.com>
2024-05-22 20:13:38 +0600

    Dark mode setup in tailwind css and active buttons setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const imgRef = useRef(null);
   const [todos, setTodos] = useState([]);
   // const [activeTodos, setActiveTodos] = useState([]);
   const [checkedTodos, setCheckedTodos] = useState([]);
   const [allFilteredTodos, setallFilteredTodos] = useState([]);
   const [completedTodos, setCompletedTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [activeFilter, setActiveFilter] = useState("");
 
-  console.log("COMPLETED TODOS ==> ", completedTodos);
-  console.log("FILTERED TODOS ==> ", allFilteredTodos);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  console.log("ACTIVE FILTER ==> ", activeFilter);
+
+  const handleInputChange = e => {
+    setInputValue(e.target.value);
+    if (e.target.value.length > 0) {
+      setErrorMsg("");
+    }
+  };
 
   const handleSubmit = event => {
     event.preventDefault();
     const form = new FormData(event.currentTarget);
-    const newTodos = [...todos, form.get("todo")];
-    setTodos(newTodos);
-    setallFilteredTodos(newTodos);
+    if (inputValue.length > 0) {
+      if (!todos.includes(inputValue)) {
+        const newTodos = [...todos, form.get("todo")];
+        setTodos(newTodos);
+        setallFilteredTodos(newTodos);
+        setInputValue("");
+      } else {
+        setErrorMsg("Your entered todo already exists");
+      }
+    } else {
+      setErrorMsg("Please enter a valid todo!");
+    }
   };
 
   // Remove todo from the list
@@ -63,11 +83,13 @@ function App() {
     if (checkedTodos.length > 0) {
       setallFilteredTodos(checkedTodos);
     }
+    setActiveFilter("active");
   };
 
   // handleAllTodos
   const handleAllTodos = () => {
     setallFilteredTodos(todos);
+    setActiveFilter("all");
   };
 
   // Handle completed todos
@@ -82,12 +104,11 @@ function App() {
         setallFilteredTodos(todoCompleted);
       }
     }
+    setActiveFilter("completed");
   };
 
   // Clear all completed todos
   const handleClearCompleted = () => {
-    console.log("ALL FILTERED TODOS", todos);
-
     const completedFilteredTodos = todos.filter(
       item => !completedTodos.includes(item)
     );
@@ -97,30 +118,89 @@ function App() {
     if (completedTodos.length > 0) {
       setCompletedTodos([]);
     }
+
+    setActiveFilter("clearCompleted");
   };
+
+  /**
+   * select the items dynamically that lefts after calculation with
+   * total number of todo list and the todos completed
+   */
+  const handleItemsLeft = () => {
+    const itemsLeft = todos.length > 0 && todos.length - completedTodos.length;
+    return itemsLeft;
+  };
+
+  /**
+   * To handle manual theme toggling
+   * From light to dark mode and vice versa
+   * Add a custom property named 'theme' in localStorage
+   * Add and remove dark and light mode according to button toggling
+   *
+   */
+  const handleToggleTheme = () => {
+    if (localStorage.theme === "dark") {
+      localStorage.theme = "light";
+      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.add("light");
+      imgRef.current.src = "/images/icon-moon.svg";
+    } else {
+      localStorage.theme = "dark";
+      document.documentElement.classList.remove("light");
+      document.documentElement.classList.add("dark");
+      imgRef.current.src = "/images/icon-sun.svg";
+    }
+  };
+
+  /**
+   * dark mode selection according to user's
+   * system or browser's preferences
+   * window.matchMedia API has been used to
+   * select the user's system dark mode
+   */
+  useEffect(() => {
+    if (window.matchMedia(`(prefers-color-scheme: dark)`).matches) {
+      localStorage.theme = "dark";
+      document.documentElement.classList.remove("light");
+      document.documentElement.classList.add("dark");
+      imgRef.current.src = "/images/icon-sun.svg";
+    }
+    setActiveFilter("all");
+  }, []);
 
   return (
     <div className="w-[480px]">
-      <header className="flex justify-between">
-        <h1>todo</h1>
-        <button type="button">
-          <img src="/images/icon-moon.svg" alt="moon icon" />
+      <header className="flex justify-between mb-10">
+        <h1 className="text-4xl uppercase font-bold text-gray-50 tracking-[12px]">
+          todo
+        </h1>
+        <button type="button" onClick={() => handleToggleTheme()}>
+          <img ref={imgRef} src="/images/icon-moon.svg" alt="moon icon" />
         </button>
       </header>
       <main>
+        {errorMsg.length > 0 ? (
+          <div className="w-full text-center p-2 tracking-wider font-semibold bg-gray-300 mb-2 rounded-sm text-sm text-red-600">
+            {errorMsg}
+          </div>
+        ) : (
+          ""
+        )}
         <form
           onSubmit={handleSubmit}
-          className="bg-white py-3 px-4 flex items-center rounded-sm shadow-sm"
+          className="bg-white py-3 px-4 flex items-center rounded-md shadow-sm dark:bg-dark-desaturated-blue-dth"
         >
           <input
             type="text"
             name="todo"
-            className="w-full outline-none border-none bg-transparent"
+            value={inputValue}
+            onChange={e => handleInputChange(e)}
+            className="w-full outline-none border-none bg-transparent dark:text-gray-50"
             placeholder="Create a new todo..."
           />
           <button
             type="submit"
-            className="ml-4 py-1 px-2 rounded-sm bg-gray-100 hover:bg-bright-blue-lth hover:text-gray-50 transition-all text-sm"
+            className="ml-4 py-1 px-2 rounded-sm bg-gray-100 dark:bg-dark-grayish-blue-ultra-dth dark:text-gray-50 dark:hover:bright-blue-lth dark:hover:text-light-gray-lth hover:bg-bright-blue-lth hover:text-gray-50 transition-all text-sm focus:ring-2 focus:ring-gradient-color-2"
           >
             Add
           </button>
@@ -128,16 +208,17 @@ function App() {
 
         {/* To do list */}
         {Array.isArray(allFilteredTodos) && allFilteredTodos.length > 0 ? (
-          <div className="my-5 bg-white shadow-lg rounded-md">
+          <div className="my-5 bg-white shadow-lg rounded-md dark:bg-dark-desaturated-blue-dth border-[1px] border-solid border-gray-50 dark:border-none">
             {allFilteredTodos.map(todo => (
               <div
                 key={todo}
-                className="flex justify-between items-center p-4 border-b-gray-200 border-b-[0.03em]"
+                className="flex justify-between items-center p-3 border-b-gray-200 border-b-[1px] dark:border-b-dark-grayish-blue-two-dth cursor-pointer group hover:bg-transparent transition duration-500"
               >
-                <div className="flex items-center">
+                <label className="flex items-center custom-checkbox">
+                  {/* Default checkbox */}
                   <input
+                    className="cursor-pointer"
                     type="checkbox"
-                    className="bg-green-500 ..."
                     onChange={event => handleCheck(event, todo)}
                     checked={
                       checkedTodos.length > 0
@@ -145,52 +226,84 @@ function App() {
                         : completedTodos.includes(todo)
                     }
                   />
+                  {/* Custom checkbox */}
+                  <span className="after:content-[''] checkmark"></span>
                   <span
-                    className={`pl-4 pr-3 ${
-                      completedTodos.includes(todo) ? "line-through" : ""
+                    className={`pl-10 pr-3 cursor-pointer dark:text-gray-200 ${
+                      completedTodos.includes(todo)
+                        ? "line-through text-gray-500 font-normal"
+                        : ""
                     }`}
                   >
                     {todo}
                   </span>
-                </div>
+                </label>
                 <button
-                  className="w-4 h-4 min-w-4"
+                  className="w-4 h-4 min-w-4 hidden group-hover:block"
                   type="button"
                   onClick={() => handleRemove(todo)}
                 >
                   <img
                     src="/images/icon-cross.svg"
                     alt={todo}
-                    className="w-full"
+                    className="w-full img-close"
                   />
                 </button>
               </div>
             ))}
             {/* Bottom settings button */}
-            <div className="flex items-center justify-between p-3 text-sm text-gray-700 font-lg">
-              <span>5 minutes left</span>
+            <div className="flex items-center justify-between p-3 text-sm text-gray-700 font-lg dark:text-gray-200">
+              <span>{`${handleItemsLeft()} items left`}</span>
               <div className="flex item-center justify-center">
-                <button type="button" onClick={() => handleAllTodos()}>
+                <button
+                  className={` ${
+                    activeFilter === "all"
+                      ? "text-bright-blue-lth font-bold"
+                      : ""
+                  }`}
+                  type="button"
+                  onClick={() => handleAllTodos()}
+                >
                   All
                 </button>
                 <button
                   type="button"
-                  className="px-3"
+                  className={`px-3 ${
+                    activeFilter === "active"
+                      ? "text-bright-blue-lth font-bold"
+                      : ""
+                  }`}
                   onClick={() => handleActive()}
                 >
                   Active
                 </button>
-                <button type="button" onClick={() => handleCompletedTodos()}>
+                <button
+                  className={`${
+                    activeFilter === "completed"
+                      ? "text-bright-blue-lth font-bold"
+                      : ""
+                  }`}
+                  type="button"
+                  onClick={() => handleCompletedTodos()}
+                >
                   Completed
                 </button>
               </div>
-              <button type="button" onClick={() => handleClearCompleted()}>
+              <button
+                className={`${
+                  activeFilter === "clearCompleted"
+                    ? "text-bright-blue-lth"
+                    : ""
+                }`}
+                type="button"
+                onClick={() => handleClearCompleted()}
+              >
                 Clear completed
               </button>
             </div>
           </div>
         ) : (
-          <div className="flex justify-between items-center p-4 border-b-gray-200 border-b-[0.03em] text-gray-600">
+          <div className="flex mt-4 justify-between items-center p-4 bg-white shadow-sm dark:bg-dark-desaturated-blue-dth rounded-md text-gray-600 dark:text-gray-50 border-[1px] border-solid border-gray-50 dark:border-none">
             No todo list added yet.
           </div>
         )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "selector",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -17,10 +18,12 @@ export default {
         "dark-blue-dth": "hsl(235, 21%, 11%)",
         "dark-desaturated-blue-dth": "hsl(235, 24%, 19%)",
         "light-grayish-blue-dth": "hsl(234, 39%, 85%)",
-        "light-grayish-blue-hvoer-dth": "hsl(236, 33%, 92%)",
+        "light-grayish-blue-hover-dth": "hsl(236, 33%, 92%)",
         "dark-grayish-blue-dth": "hsl(234, 11%, 52%)",
         "dark-grayish-blue-ultra-dth": "hsl(233, 14%, 35%)",
         "dark-grayish-blue-two-dth": "hsl(237, 14%, 26%)",
+        "gradient-color-1": "hsl(192, 100%, 67%)",
+        "gradient-color-2": "hsl(280, 87%, 65%)",
       },
       fontFamily: {
         "font-josefin": "'Josefin Sans', sans-serif",
